badcc60a9506234a791b5d6383ed206c32a3d5f9
codehub-py <imvirenjalamkar2005@gmail.com>
2023-11-13 16:13:34 +0530

    Task4

---
+++ Task4.py
@@ -0,0 +1,21 @@
+'''4) Create a python function that takes two arguments as inputs. The input arguments must
+be integers (ensure this using python). The function should display a multiplication of
+input integers to the user.'''
+#Solution :-
+def multiplication():
+
+    # "x*y" this is the formula for multiplication
+    print(f'Total :- {x * y}')
+
+# Take 2 integer valu as input
+print("Multiplication of 2 integer numbers")
+
+# valu 1 save in variable "x"
+x = int(input("Enter valu 1 :- "))
+
+ # valu 2 save in variable "y"
+y = int(input("Enter valu 2 :- "))
+
+
+multiplication()
+
